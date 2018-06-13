f875b2e3ebf1ba44b0b5d1706ad668f811bfa555
BearandYoon <luckysevenbear@gmail.com>
2018-06-13 20:55:39 +0200

    - implemented service test

--- src/app/mock_backend/db.ts
+++ src/app/mock_backend/db.ts
@@ -1,75 +1,73 @@
-export const db = [
-  {
-    etag: 'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/1bBuvoRES2RbSsm8zrrFEToKhaE',
-    kind: 'youtube#searchListResponse',
-    nextPageToken: 'CDIQAA',
-    items: [
-      {
-        'kind': 'youtube#searchResult',
-        'etag': '\'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/QtJ4MlYKdN_zTBjfY3xY6mn7ZRg\'',
-        'id': {
-          'kind': 'youtube#video',
-          'videoId': '3fumBcKC6RE'
-        },
-        'snippet': {
-          'publishedAt': '2011-05-12T20:01:31.000Z',
-          'channelId': 'UCEOhcOACopL42xyOBIv1ekg',
-          'title': 'Lil Wayne - John (Explicit) ft. Rick Ross',
-          'description': 'Music video by Lil Wayne performing John. (C) 2011 Cash Money Records Inc.',
-          'thumbnails': {
-            'default': {
-              'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/default.jpg',
-              'width': 120,
-              'height': 90
-            },
-            'medium': {
-              'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/mqdefault.jpg',
-              'width': 320,
-              'height': 180
-            },
-            'high': {
-              'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/hqdefault.jpg',
-              'width': 480,
-              'height': 360
-            }
-          },
-          'channelTitle': 'LilWayneVEVO',
-          'liveBroadcastContent': 'none'
-        }
+export const db = {
+  etag: 'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/1bBuvoRES2RbSsm8zrrFEToKhaE',
+  kind: 'youtube#searchListResponse',
+  nextPageToken: 'CDIQAA',
+  items: [
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '\'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/QtJ4MlYKdN_zTBjfY3xY6mn7ZRg\'',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': '3fumBcKC6RE'
       },
-      {
-        'kind': 'youtube#searchResult',
-        'etag': '\'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/pRlb6OnS5jGoiGdwuOT_OXsdwjA\'',
-        'id': {
-          'kind': 'youtube#video',
-          'videoId': 'nG2pEffLEJo'
-        },
-        'snippet': {
-          'publishedAt': '2018-06-04T06:30:00.000Z',
-          'channelId': 'UC3XTzVzaHQEd30rQbuvCtTQ',
-          'title': 'Guardianship: Last Week Tonight with John Oliver (HBO)',
-          'description': 'It is alarmingly easy for a total stranger to gain total control over the life of a senior citizen.',
-          'thumbnails': {
-            'default': {
-              'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/default.jpg',
-              'width': 120,
-              'height': 90
-            },
-            'medium': {
-              'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/mqdefault.jpg',
-              'width': 320,
-              'height': 180
-            },
-            'high': {
-              'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/hqdefault.jpg',
-              'width': 480,
-              'height': 360
-            }
+      'snippet': {
+        'publishedAt': '2011-05-12T20:01:31.000Z',
+        'channelId': 'UCEOhcOACopL42xyOBIv1ekg',
+        'title': 'Lil Wayne - John (Explicit) ft. Rick Ross',
+        'description': 'Music video by Lil Wayne performing John. (C) 2011 Cash Money Records Inc.',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/default.jpg',
+            'width': 120,
+            'height': 90
           },
-          'channelTitle': 'LastWeekTonight',
-          'liveBroadcastContent': 'none'
-        }
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/3fumBcKC6RE/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'LilWayneVEVO',
+        'liveBroadcastContent': 'none'
       }
-    ]
-  }
-];
+    },
+    {
+      'kind': 'youtube#searchResult',
+      'etag': '\'DuHzAJ-eQIiCIp7p4ldoVcVAOeY/pRlb6OnS5jGoiGdwuOT_OXsdwjA\'',
+      'id': {
+        'kind': 'youtube#video',
+        'videoId': 'nG2pEffLEJo'
+      },
+      'snippet': {
+        'publishedAt': '2018-06-04T06:30:00.000Z',
+        'channelId': 'UC3XTzVzaHQEd30rQbuvCtTQ',
+        'title': 'Guardianship: Last Week Tonight with John Oliver (HBO)',
+        'description': 'It is alarmingly easy for a total stranger to gain total control over the life of a senior citizen.',
+        'thumbnails': {
+          'default': {
+            'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/default.jpg',
+            'width': 120,
+            'height': 90
+          },
+          'medium': {
+            'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/mqdefault.jpg',
+            'width': 320,
+            'height': 180
+          },
+          'high': {
+            'url': 'https://i.ytimg.com/vi/nG2pEffLEJo/hqdefault.jpg',
+            'width': 480,
+            'height': 360
+          }
+        },
+        'channelTitle': 'LastWeekTonight',
+        'liveBroadcastContent': 'none'
+      }
+    }
+  ]
+};
--- src/app/services/app.service.spec.ts
+++ src/app/services/app.service.spec.ts
@@ -1,19 +1,35 @@
 import { TestBed, inject } from '@angular/core/testing';
-
-import { AppService } from './app.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+import { AppService } from './app.service';
+import { MockHttpClient } from '../mock_backend/mock-http-client';
+
 describe('AppService', () => {
+  let serviceInstance: AppService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       providers: [
-        AppService
+        AppService,
+        {provide: HttpClient, useClass: MockHttpClient}
       ]
     });
+  });
+
+  beforeEach(async() => {
+    serviceInstance = TestBed.get(AppService);
   });
 
   it('should be created', inject([AppService], (service: AppService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('getGridData api should get video list as observable array, and first videoId should be 3fumBcKC6RE', (done: DoneFn) => {
+    serviceInstance.getGridData().subscribe((res: Array<any>) => {
+      expect(res.length).toEqual(2);
+      expect(res[0].id.videoId).toEqual('3fumBcKC6RE');
+      done();
+    });
+  });
 });
