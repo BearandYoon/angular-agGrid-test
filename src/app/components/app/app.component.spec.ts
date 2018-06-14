ffd18a3f86c76b20b5ce8447414db19eb4147ddd
BearandYoon <luckysevenbear@gmail.com>
2018-06-14 11:33:21 +0200

    - implemented appComponent Testing

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -1,12 +1,63 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { AgGridModule } from 'ag-grid-angular';
+import { UiSwitchModule } from 'ngx-ui-switch';
+
 import { AppComponent } from './app.component';
+import { VideoThumbnailRendererComponent } from '../video-thumbnail-renderer/video-thumbnail-renderer.component';
+import { AppService } from '../../services/app.service';
+import { db } from '../../mock_backend/db';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let appService: AppService;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        HttpClientModule,
+        AgGridModule.forRoot(),
+        UiSwitchModule
       ],
-    }).compileComponents();
+      declarations: [
+        AppComponent,
+        VideoThumbnailRendererComponent
+      ],
+      providers: [
+        AppService
+      ]
+    })
+    .compileComponents();
   }));
+
+  beforeEach(() => {
+    appService = TestBed.get(AppService);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('After transformData function, totalCount should be 2', () => {
+    const data = db.items;
+    const res = component.transformData(data);
+    expect(component.totalCounts).toEqual(2);
+  });
+
+  it('After transformData function, response should be new format', () => {
+    const data = db.items;
+    const transformedData = component.transformData(data);
+    expect(transformedData[0].videoId).toBeDefined();
+    expect(transformedData[0].thumbnail).toBeDefined();
+    expect(transformedData[0].publishedAt).toBeDefined();
+    expect(transformedData[0].title).toBeDefined();
+    expect(transformedData[0].description).toBeDefined();
+  });
 });
